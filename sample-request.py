adcbf2a0f7fc2d0a99d3222e044f53a5aa3918e4
mukund kalantri <mukundkalantri23@gmail.com>
2022-12-07 20:58:09 -0700

    hpa complete

--- sample-request.py
+++ sample-request.py
@@ -2,22 +2,23 @@ import base64
 import json                    
 import requests
 
-api = 'http://localhost/get_min_shops'
-image_file = 'SampleReciept1.png'
+for i in range(100):
+    api = 'http://localhost/get_min_shops'
+    image_file = 'SampleReciept1.png'
 
-with open(image_file, "rb") as f:
-    im_bytes = f.read()        
-im_b64 = base64.b64encode(im_bytes).decode("utf8")
+    with open(image_file, "rb") as f:
+        im_bytes = f.read()        
+    im_b64 = base64.b64encode(im_bytes).decode("utf8")
 
-headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-  
-payload = json.dumps({"image": im_b64, "email_id": "muka4041@colorado.edu"})
-response = requests.post(api, data=payload, headers=headers)
-try:
-    data = response.json()     
-    print(data)                
-except requests.exceptions.RequestException:
-    print(response.text)
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    
+    payload = json.dumps({"image": im_b64, "email_id": "muka4041@colorado.edu"})
+    response = requests.post(api, data=payload, headers=headers)
+    try:
+        data = response.json()     
+        print(data)                
+    except requests.exceptions.RequestException:
+        print(response.text)
 
 # api = 'http://localhost/sql'
 
